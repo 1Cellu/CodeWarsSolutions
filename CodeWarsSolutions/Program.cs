7fad998ec91ef45965c46b972af396b284bde522
Daniel Cortes <danielcortes40@hotmail.com>
2022-10-25 20:53:19 -0300

    Informations in classes were inverted. Solution to problem You only need one made.

--- CodeWarsSolutions/Program.cs
+++ CodeWarsSolutions/Program.cs
@@ -1,156 +1,51 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+using Kata;
 
-namespace Kata
+namespace Program
 {
-    public class Kata
+    class Program
     {
+        static void Main(string[] args)
+        {
+            #region You only need one
 
-        #region Fix My Phone Number
-        //public String IsItANum(string str)
-        //{
-        //    var arr = str.ToCharArray();
-        //    List<char> list = new List<char>();
+            //var instance = new Kata.Kata();
+            //string[] arr = { "a", "b", "c"};
+            //instance.Check(arr, "a");
+            //Console.WriteLine(instance.Check(arr, "a"));
 
-        //    for (int i = 0; i < arr.Length; i++)
-        //    {
-        //        if (Char.IsDigit(arr[i])) list.Add(arr[i]);
-        //    }
+            #endregion
 
-        //    return list.Count == 11 && list.IndexOf('0') == 0 ? string.Join("", list.ToArray()) : "Not a phone number";
-        //}
+            #region Fix My Phone Number
 
+            //var instance = new Kata.Kata();
+            //instance.IsItANum("S:)0207ERGQREG88349F82!efRF)");
+            //Console.ReadLine();
 
-        #endregion
+            #endregion
 
-        #region Funny Dots
-        //public string Dot(int n, int m)
-        //{
-        //    var stringConcateneted = "";
+            #region Funny Dots
 
-        //    var firstPoint = true;
-        //    if (firstPoint)
-        //    {
-        //        stringConcateneted += String.Concat(Enumerable.Repeat("+---", n)) + "+\n" +
-        //                              String.Concat(Enumerable.Repeat("| o ", n)) + "|\n" +
-        //                              String.Concat(Enumerable.Repeat("+---", n)) + "+";
+            //Kata.Kata instance = new Kata.Kata();
+            //var n = 1;
+            //var m = 2;
 
-        //        Console.WriteLine(stringConcateneted);
-        //        firstPoint = false;
-        //    }
+            //instance.Dot(n, m);
 
-        //    if (!firstPoint)
-        //    {
-        //        stringConcateneted += String.Concat(Enumerable.Repeat("\n" + String.Concat(Enumerable.Repeat("| o ", n)) + "|\n" +
-        //                              String.Concat(Enumerable.Repeat("+---", n)) + "+", (m - 1)));
+            //Console.ReadLine();
+            #endregion
 
-        //        Console.WriteLine(stringConcateneted);
-        //    }
-        //    return String.Empty;
-        //}
-        #endregion
+            #region +1Array 
 
-        #region +1Array
-        //public int[] UpArray1(int[] num)
-        //{
+            //Kata.Kata instance = new Kata.Kata();
+            //var arr = new int[] { 9, 3, 6, 4, 1, 2 };     
+            //Console.WriteLine(instance.UpArray1(arr));
+            //Console.WriteLine(instance.UpArray2(arr));
+            //Console.WriteLine(instance.UpArray3(arr));
+            //Console.ReadLine();
 
-        //    var total = "";
-        //    if (num != null)
-        //    {
-        //        for (int i = 0; i < num.Length; i++)
-        //        {
-        //            if (num[i] >= 0 && num[i] <= 10)
-        //            {
-        //                total += num[i].ToString();
-        //            }
-        //            else
-        //            {
-        //                return null;
-        //            }
-        //        }
-        //    }
-        //    else
-        //    {
-        //        return null;
-        //    }
-        //    if (total != null && total != "")
-        //    {
-        //        var arrayResult = total.ToString().Select(o => Convert.ToInt32(o) - 48).ToArray();
-        //        if (arrayResult[arrayResult.Length - 1] == 9)
-        //        {
-        //            if (int.TryParse(total, System.Globalization.NumberStyles.HexNumber, null, out int i))
-        //            {
-        //                var result = Convert.ToInt64(total) + 1;
-        //                arrayResult = result.ToString().Select(o => Convert.ToInt32(o) - 48).ToArray();
-        //            }
-        //            else
-        //            {
-        //                arrayResult[arrayResult.Length - 1] = 0;
-        //                arrayResult[arrayResult.Length - 2] = arrayResult[arrayResult.Length - 2] + 1;
-        //            }
-        //        }
-        //        else
-        //        {
-        //            arrayResult[arrayResult.Length - 1] = arrayResult[arrayResult.Length - 1] + 1;
-        //        }
-        //        return arrayResult;
-        //    }
-        //    return null;
-        //}
-
-        //public int[] UpArray2(int[] num)
-        //{
-        //    if (num.Length == 0 || num.Any(a => a < 0 || a > 9))
-        //    {
-        //        return null;
-        //    }
-
-        //    for (var i = num.Length - 1; i >= 0; i--)
-        //    {
-        //        if (num[i] == 9)
-        //        {
-        //            num[i] = 0;
-        //        }
-        //        else
-        //        {
-        //            num[i]++;
-        //            return num;
-        //        }
-        //    }
-        //    return new[] { 1 }.Concat(num).ToArray();
-        //}
-
-        //public int[] UpArray3(int[] num)
-        //{
-        //    string joinedNumber = string.Join(string.Empty, num);
-        //    if (num.Length == 0 || joinedNumber.Length != num.Length)
-        //    {
-        //        // Means num was empty or there was a '-' or an integer greater than 9 in num
-        //        return null;
-        //    }
-
-        //    List<int> result = new List<int>();
-
-        //    // Starting with one to add the one
-        //    int carryover = 1;
-        //    for (int i = num.Length - 1; i >= 0; i--)
-        //    {
-        //        int value = num[i] + carryover;
-        //        carryover = value / 10;
-        //        value %= 10;
-
-        //        result.Insert(0, value);
-        //    }
-
-        //    if (carryover > 0)
-        //    {
-        //        result.Insert(0, carryover);
-        //    }
-
-        //    return result.ToArray();
-        //}
-
-        #endregion
+            #endregion
+        }
     }
 }
+
